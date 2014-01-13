b0140d06f2de0d1fb38343db56a89b05aa70b162
Ian Hartwig <ihartwig@andrew.cmu.edu>
2014-01-13 17:17:41 -0500

    also add avr-libc

--- avr-gcc.rb
+++ avr-gcc.rb
@@ -12,7 +12,7 @@ class AvrGcc < Formula
   url 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2'
   # sha1 'a464ba0f26eef24c29bcd1e7489421117fb9ee35'
 
-  depends_on 'ihartwig/homebrew-formula/avr-binutils'
+  depends_on 'ihartwig/formula/avr-binutils'
   depends_on 'gmp'
   depends_on 'libmpc'
   depends_on 'mpfr'
